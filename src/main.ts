16a35f57440c0e2c303acbeecf0f1e4cfed75cf0
Caleb Van Lue <vanluecaleb@outlook.com>
2025-05-07 22:25:07 -0400

    enable cors for localhost

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,11 @@ async function bootstrap() {
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
