af97be03900ed6758e2aa241e8cbb1e4f9b65edc
Caleb Van Lue <vanluecaleb@outlook.com>
2025-05-10 18:00:32 -0400

    ensure nestjs doesn't just stick to localhost

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,6 @@ async function bootstrap() {
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
